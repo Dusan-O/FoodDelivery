31624793e249d80fe1a577534fd579cd2565d2ce
Dusan-O <dusan.orescanin@gmail.com>
2022-08-31 19:13:32 +0200

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Dusan Orescanin on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
